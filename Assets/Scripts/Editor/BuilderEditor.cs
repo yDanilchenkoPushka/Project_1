78130a04d1b728444ddf3de60884b5e07824e495
PUSHKA\y.danilchenko <y.danilchenko@pushkastudios.com>
2022-08-15 08:03:57 -0700

    Add building from script.

---
+++ Assets/Scripts/Editor/BuilderEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace DefaultNamespace.Editor
+{
+    public class BuilderEditor
+    {
+        [MenuItem("Buildings/Standalone")]
+        public static void RunStandaloneBuild()
+        {
+            string fileName = "TestBuild";
+            string fileExtension = ".exe";
+            
+            string dataPath = EditorUtility.SaveFolderPanel("Windows Build", "", "");
+
+            string locationStringPathName = dataPath + "/" + fileName + fileExtension;
+
+            string[] scenePaths = new[] { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/Level.unity" };
+            
+            BuildPipeline.BuildPlayer(scenePaths, locationStringPathName, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        }
+    }
+}
